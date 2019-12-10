5a3380a4051bbc1d16fb66940fb10a41d6e8ac5c
xy <zhangxuyi@wanshare.com>
2019-12-10 23:03:28 +0800

    add bridge

---
+++ bridge/Device.java
@@ -0,0 +1,13 @@
+package com.dp.bridge;
+
+
+interface Device {
+    public int getVolumn();
+    public boolean setVolumn();
+    public int getChannel();
+    public boolean setChannel();
+
+
+}
+
+
